7e4bb6d15f45f8b432e5146e4d53e90fcdad8d8b
xi <xi@shaoli.cn>
2024-02-26 22:52:47 +0800

    test add

---
+++ idea_test/src/com/itheima_ys/Ys.java
@@ -0,0 +1,25 @@
+package com.itheima_ys;
+
+/**
+ * @PROJECT_NAME: JavaSE_Code
+ * @DESCRIPTION:
+ * @USER: fujione
+ * @DATE: 2024/2/22 15:38
+ */
+public class Ys {
+
+    public static void main(String[] args) {
+        int bcs = 28;
+        int cs = 7;
+        int i = 0;
+        while (bcs>=cs){
+            bcs = bcs - cs;
+            i++;
+        }
+
+        System.out.println("商为"+i+",余数为"+bcs);
+
+
+
+    }
+}
